66343adbf3871afc927654c78d445f632e1f0a55
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2024-03-25 20:10:03 +0500

    Error removing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="w-full h-screen text-white px8">
+      <nav className="flex items-center justify-center p-3">
+        <h1 className="text-3xl font-bold tracking-wide">Weather App</h1>
+        <div className="bg-white w-[15rem] overflow-hidden shadow-2xl rounded flex items-center p-2 gap-2">
+          <img src={} alt="search" />
+          <input type="text" className=" focus:outline-none w-full text-[#212121] text-lg" />
+        </div>        
+      </nav>
+    </div>
   )
 }
