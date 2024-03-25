f589d18d302add54b3643d5d165042c581497103
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2024-03-26 02:30:39 +0500

    Building Search

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
+import { useState } from "react"
+import Search from "./assets/icons/search.svg";
+
 export default function App() {
+
+  const [input, setInput] = useState("");
+
   return (
     <div className="w-full h-screen text-white px8">
       <nav className="flex items-center justify-center p-3">
         <h1 className="text-3xl font-bold tracking-wide">Weather App</h1>
         <div className="bg-white w-[15rem] overflow-hidden shadow-2xl rounded flex items-center p-2 gap-2">
-          <img src={} alt="search" />
-          <input type="text" className=" focus:outline-none w-full text-[#212121] text-lg" />
+          <img 
+          src={Search} alt="search"
+          className="w-[1.5rem] h-[1.5rem] "
+           />
+          <input type="text" 
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          className=" focus:outline-none w-full text-[#212121] text-lg" />
         </div>        
       </nav>
     </div>
